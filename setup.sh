8a0f10c9a8b07cf657bec9124afc5b81f5240257
duyphuong6132 <phuonglepheo@gmail.com>
2025-02-14 10:31:34 +0700

    first commit

--- setup.sh
+++ setup.sh
@@ -13,9 +13,11 @@ yay -S --needed foot alacritty lsd bat tmux neovim tldr vlc yazi ranger \
 yay -S --needed mesa vulkan-intel intel-media-driver libva-intel-driver xf86-video-nouveau vulkan-mesa-layers lib32-mesa vulkan-radeon
 
 # Install scripts
+cd dotfiles2
 cd Arch-Hyprland 
 chmod +x install.sh
 ./install.sh
+cd ..
 
 # Copy dotfiles
 cp -r .config ~/ 
@@ -23,4 +25,4 @@ cp .zshrc ~/
 cp .tmux.conf ~/ 
 cp -r .themes ~/ 
 cp -r .icons ~/ 
-
+cp -r Pictures ~/ 
